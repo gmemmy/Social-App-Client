25729603cc4f8c2341421e16b4d433f595091586
Emmy <gmemmy22@gmail.com>
2019-07-25 16:59:20 +0100

    Create NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Social App</h1>
-    </div>
-  );
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signUp from "./pages/signup";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signUp} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class signUp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sign Up Page</h1>
+      </div>
+    )
+  }
+}
+
+export default signUp;
