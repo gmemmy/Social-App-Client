78c99f064ea2ddfe8e7e54fcac59e16f347349c1
Emmy <gmemmy22@gmail.com>
2019-07-26 11:54:32 +0100

    Create card component for user feed

---
+++ src/components/Post.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Link from "react-router-dom/Link";
+
+// MUI Stuff
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
+  card: {
+    display: "flex",
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover'
+  }
+};
+
+class Post extends Component {
+  render() {
+    const {
+      classes,
+      post: {
+        body,
+        createdAt,
+        userImage,
+        username,
+        postId,
+        likeCount,
+        commentCount
+      }
+    } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image="https://firebasestorage.googleapis.com/v0/b/social-app-d3eeb.appspot.com/o/no-img.png?alt=media"
+          title="Profile Image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${username}`}
+            color="primary"
+          >
+            {username}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {createdAt}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+export default withStyles(styles)(Post);
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
 
- class home extends Component {
+import Post from '../components/Post';
+
+class home extends Component {
+  state = {
+    posts: null
+  };
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
   render() {
+    let recentPostsMarkup = this.state.posts ? (
+      this.state.posts.map(post => <Post post={post} />)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
-    )
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {recentPostsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
