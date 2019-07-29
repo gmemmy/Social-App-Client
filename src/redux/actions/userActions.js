b4238e7810f6bbf343d3d5c285498a34844a6d81
Emmy <gmemmy22@gmail.com>
2019-07-29 23:35:13 +0100

    Create action for logging out users

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from "../types";
 
 export const signupUser = (newUserData, history) => dispatch => {
   dispatch({ type: LOADING_UI });
@@ -36,6 +36,12 @@ export const loginUser = (userData, history) => dispatch => {
       });
     });
 };
+
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem('FBIdToken');
+  delete axios.defaults.headers.common['Authorization'];
+  dispatch({ type: SET_UNAUTHENTICATED });
+}
 
 export const getUserData = () => dispatch => {
   axios
