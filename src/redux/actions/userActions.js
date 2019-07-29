b1ec868c93ba4febe0d8954be6d8ff258914882b
Emmy <gmemmy22@gmail.com>
2019-07-29 23:28:05 +0100

    Create sign up action and refactor code

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,14 +1,30 @@
 import axios from "axios";
 import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
 
+export const signupUser = (newUserData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/signup", newUserData)
+    .then(res => {
+      setAuthrizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
 export const loginUser = (userData, history) => dispatch => {
   dispatch({ type: LOADING_UI });
   axios
     .post("/login", userData)
     .then(res => {
-      const FBIdToken = `Bearer ${res.data.token}`;
-      localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
-      axios.defaults.headers.common["Authorization"] = FBIdToken;
+      setAuthrizationHeader(res.data.token);
       dispatch(getUserData());
       dispatch({ type: CLEAR_ERRORS });
       history.push("/");
@@ -33,4 +49,10 @@ export const getUserData = () => dispatch => {
     .catch(err => {
       console.log(err);
     });
+};
+
+const setAuthrizationHeader = token => {
+  const FBIdToken = `Bearer ${token}`;
+  localStorage.setItem("FBIdToken", `Bearer ${token}`);
+  axios.defaults.headers.common["Authorization"] = FBIdToken;
 };
