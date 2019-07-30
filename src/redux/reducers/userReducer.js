11e8acbf37d8433d2c5df74b9e4d7ab0b8360779
Emmy <gmemmy22@gmail.com>
2019-07-30 16:59:34 +0100

    Create reducr functions to handle like and unlike action types

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -2,7 +2,9 @@ import {
   SET_USER,
   SET_AUTHENTICATED,
   SET_UNAUTHENTICATED,
-  LOADING_USER
+  LOADING_USER,
+  LIKE_POST,
+  UNLIKE_POST
 } from "../types";
 
 const initialState = {
@@ -32,6 +34,22 @@ export default function(state = initialState, action) {
       return {
         ...state,
         loading: true
+      };
+    case LIKE_POST:
+      return {
+        ...state,
+        likes: [
+          ...state.likes,
+          {
+            username: state.credentials.username,
+            postId: action.payload.postId
+          }
+        ]
+      };
+    case UNLIKE_POST:
+      return {
+        ...state,
+        likes: state.likes.filter(like => like.postId === action.payload.postId)
       };
     default:
       return state;
