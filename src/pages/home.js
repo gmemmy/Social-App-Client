59bcf302411878a3d21aee8ef54af622e32c8fd3
Emmy <gmemmy22@gmail.com>
2019-07-30 17:15:41 +0100

    Refactor home component to fetch state from redux store

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,23 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
 
 import Post from "../components/Post";
 import Profile from "../components/Profile";
 
+// Redux Stuff
+import { connect } from "react-redux";
+import { getPosts } from "../redux/actions/dataActions";
+
 class home extends Component {
-  state = {
-    posts: null
-  };
   componentDidMount() {
-    axios
-      .get("/posts")
-      .then(res => {
-        this.setState({
-          posts: res.data
-        });
-        console.log(res.data);
-      })
-      .catch(err => console.log(err));
+    this.props.getPosts();
   }
   render() {
-    let recentPostsMarkup = this.state.posts ? (
-      this.state.posts.map(post => <Post post={post} key={post.postId} />)
+    const { posts, loading } = this.props.data;
+    let recentPostsMarkup = !loading ? (
+      posts.map(post => <Post post={post} key={post.postId} />)
     ) : (
       <p>Loading...</p>
     );
@@ -39,4 +34,20 @@ class home extends Component {
   }
 }
 
-export default home;
+home.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+const mapActionsToProps = {
+  getPosts
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(home);
