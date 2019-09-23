52545c3948876e386964d0386cd2116e973bb59e
Emmy <nuelojay22@gmail.com>
2019-09-23 09:40:38 +0100

    Create page that allows a user view other users' profile and their posts

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,13 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Post from "../components/post/Post";
+import StaticProfile from '../components/profile/StaticProfile';
+import Grid from "@material-ui/core/Grid";
+
+import { connect } from "react-redux";
+import { getUserData } from "../redux/actions/dataActions";
 
 export class User extends Component {
+  state = {
+    profile: null
+  };
+  componentDidMount() {
+    const username = this.props.match.params.username;
+    this.props.getUserData(username);
+    axios
+      .get(`/user/${username}`)
+      .then(res => {
+        this.setState({
+          profile: res.data.user
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
   render() {
+    const { posts, loading } = this.props.data;
+    const postsMarkup = loading ? (
+      <p>Loading data...</p>
+    ) : posts === null ? (
+      <p>No posts from this user</p>
+    ) : (
+      posts.map(post => <Post key={post.postId} post={post} />)
+    );
     return (
-      <div>
-        Hello there
-      </div>
-    )
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {postsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+         {this.state.profile === null ? (
+           <p>Loading Profile...</p>
+          ) : <StaticProfile profile={this.state.profile} />
+          }
+        </Grid>
+      </Grid>
+    );
   }
 }
 
-export default User;
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+User.propTypes = {
+  getUserData: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  { getUserData }
+)(User);
